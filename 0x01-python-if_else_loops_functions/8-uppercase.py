c9ea2c14a3d1cbcc43b209e8ccd86d4784f41f44
Chura33 <churabrady@gmail.com>
2023-06-06 14:28:43 -0700

    solved 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for i in range(len(str)):
+        res = ord(str[i])
+        if res >= 97:
+            res -= 32
+        print("{res:c}".format(res=res), end="")
+    print("\n")
