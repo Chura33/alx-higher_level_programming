d54d153a554c178f0fed09dcf6efcf09615c451f
Chura33 <churabrady@gmail.com>
2023-06-06 11:04:25 -0700

    changed print to .format() stule for 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i == 99:
-        print(f"{i}\n")
+        print("{i}\n".format(i=i))
     else:
-        print(f"{i}", end=", ")
+        print("{i}".format(i=i), end=", ")
