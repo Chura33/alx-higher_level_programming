bcf1074333b20a39b58837a647fb4867e2de9250
Chura33 <churabrady@gmail.com>
2023-06-06 11:17:50 -0700

    removed new line character 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i == 99:
-        print("{i}\n".format(i=i))
+        print("{i}".format(i=i))
     else:
         print("{i:02}".format(i=i), end=", ")
