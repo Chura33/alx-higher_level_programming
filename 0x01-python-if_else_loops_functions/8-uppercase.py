9fe554a803bb8d22fa3dcad4db3bf11602cb5e68
Chura33 <churabrady@gmail.com>
2023-06-06 14:56:08 -0700

    resolved end of string spacing issue

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,6 +6,6 @@ def uppercase(str):
         if res >= 97:
             res -= 32
         if i == len(str) - 1:
-            print("{res:c} ".format(res=res))
+            print("{res:c}".format(res=res))
         else:
             print("{res:c}".format(res=res), end="")
