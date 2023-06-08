cfcbf831a022698bb48ed42e489140feb49ae0fa
Chura33 <churabrady@gmail.com>
2023-06-08 15:33:12 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    from sys import argv
+    count = len(argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, argv[i + 1]))
