8962df5c9c5f750cfec001dffbfaee81764d75de
Chura33 <churabrady@gmail.com>
2023-06-14 15:49:02 -0700

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    array = a_dictionary.keys()
+    array = list(a_dictionary.keys())
     largest = a_dictionary[array[0]]
     largest_key = array[0]
     for i in array:
         if a_dictionary[i] > largest:
             largest = a_dictionary[i]
-	    largest_key = i
-    if (largest and largest_key):
-        return (largest_key)
-    return (None)
+            largest_key = i
+    if largest_key:
+        return largest_key
+    return None
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
 
