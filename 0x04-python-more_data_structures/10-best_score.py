7e1bfdefc9dffea8d69b9839e28e34b557395dbb
Chura33 <churabrady@gmail.com>
2023-06-14 15:52:02 -0700

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -10,8 +10,3 @@ def best_score(a_dictionary):
     if largest_key:
         return largest_key
     return None
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
