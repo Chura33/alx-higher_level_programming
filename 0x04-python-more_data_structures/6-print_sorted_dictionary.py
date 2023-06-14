9db22d8bb84547caabe44fba32a623719953f935
Chura33 <churabrady@gmail.com>
2023-06-14 13:58:15 -0700

    corrected the return statement to a print

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    return ({x: a_dictionary[x] for x in sorted(a_dictionary)})
+    sorted_dict = {x: a_dictionary[x] for x in sorted(a_dictionary)}
+    for i in sorted_dict.keys():
+        print("{}: {}".format(i, sorted_dict[i]))
