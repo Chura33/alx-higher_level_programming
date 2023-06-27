bb411cfde35baa76f3a770563fbe46da3add54cb
Chura33 <churabrady@gmail.com>
2023-06-27 12:16:06 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 
+"""Define a Square class"""
+
+
 class Square:
+    """Blueprint for a square"""
+
     def __init__(self, size):
+        """Initialize a new square
+
+        Args:
+            size (int): The length of the square'
+        """
         self.__size = size
