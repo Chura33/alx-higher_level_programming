b851e2a716c3f5ffd248710a6600c668fc356e54
Chura33 <churabrady@gmail.com>
2023-07-03 18:12:15 -0700

    created 0-rectangle.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 """Define a class Square."""
-
-
-
 class Square:
 
     """Represent a square."""
@@ -12,17 +9,10 @@ class Square:
     def __init__(self, size):
 
         """Initialize a new square.
-
-
         Args:
-
             size (int): The size of the new square.
-
         """
-
         self.size = size
-
-
     @property
 
     def size(self):
@@ -52,18 +42,12 @@ class Square:
         """Return the current area of the square."""
 
         return (self.__size * self.__size)
-
-
     def my_print(self):
 
         """Print the square with the # character."""
-
         for i in range(0, self.__size):
 
             [print("#", end="") for j in range(self.__size)]
-
             print("")
-
         if self.__size == 0:
-
             print("")
