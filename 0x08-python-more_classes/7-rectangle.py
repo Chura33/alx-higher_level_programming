97bdcfbf3d52bc8d1b12c241e045e6b74ba51fed
Chura33 <churabrady@gmail.com>
2023-07-03 23:33:06 -0700

    added self. before the print symbol in order to use the print symbol

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -70,7 +70,7 @@ class Rectangle:
 
         rect = []
         for i in range(self.__height):
-            [rect.append(str(print_symbol)) for j in range(self.__width)]
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
