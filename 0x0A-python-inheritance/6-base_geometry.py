65a1c75ae9001c75d7979d223e63e190959db283
Chura33 <churabrady@gmail.com>
2023-07-10 15:52:30 -0700

    corrected the blank line errors

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,9 +5,7 @@
 class BaseGeometry:
     """ a class that has a method which raises an error """
 
-
     def area(self):
         """ This function raises an error"""
-
 
         raise Exception("area() is not implemented")
