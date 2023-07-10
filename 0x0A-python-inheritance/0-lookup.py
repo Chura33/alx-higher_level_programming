e0db13de5efd14316e354926dcd8102a396e17ea
Chura33 <churabrady@gmail.com>
2023-07-10 14:26:35 -0700

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module 0-lookup
+Returns a list of the attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """ Returns the lists of attributes and methods of object obj
+    Args:
+        obj (_type_): object
+    Returns:
+        dir(obj)
+    """
+    return (dir(obj))
