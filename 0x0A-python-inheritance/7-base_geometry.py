e3231bc898e77512b28f7326e9de0dd4bf06910c
Chura33 <churabrady@gmail.com>
2023-07-10 16:06:04 -0700

    corrected indentation issues in 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,10 +7,11 @@ class BaseGemoetry:
         """This method simply raises an exception"""
 
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
 
-    """ This validates the value """
-    if type(value) != int:
-        raise TypeError("{} must be an integer".format(name))
-    if value <= 0:
-        raise ValueError("{} must be greater than 0".format(name))
+        """ This validates the value """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
