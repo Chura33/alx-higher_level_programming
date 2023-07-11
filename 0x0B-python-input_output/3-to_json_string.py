aa3c1c5c77eff0aa3b107f25d323c5d4765df003
Chura33 <churabrady@gmail.com>
2023-07-11 16:08:11 -0700

    solved tasks 0 to 7

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module 3-to_json_string.
+Returns the JSON of an object.
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON of my_obj.
+    Args:
+        - my_obj: string to represent
+    Returns: JSON representation
+    """
+
+    return json.dumps(my_obj)
