28905ac166da0134686f2f14dedb76d3c5ec68ac
Chura33 <churabrady@gmail.com>
2023-07-17 14:04:04 -0700

    created base.py and __init__.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,13 +2,16 @@
 
 """ defines a base model class"""
 
+
 class Base:
     """Base model.
     This Represents the "base" for all other classes in project 0x0C*.
     Private Class Attributes:
         __nb_object (int): Number of instantiated Bases.
     """
+
     __nb_objects = 0
+
     def __init__(self, id=None):
         """Initialize a new Base.
         Args:
