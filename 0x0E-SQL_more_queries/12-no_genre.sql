6e53533e6618bdcd8b681efd26fd75125602b3d9
Chura33 <churabrady@gmail.com>
2023-08-18 04:23:17 -0700

    show tv shows with no genre id

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -6,5 +6,6 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows 
 LEFT JOIN tv_show_genres
-ON tv_show_genres.genre_id IS NULL
+ON tv_show_genres.show_id=tv_shows.id
+WHERE tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
