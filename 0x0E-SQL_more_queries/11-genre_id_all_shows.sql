ca4d3b3366d22b40ec32cb5adf50c518733d6657
Chura33 <churabrady@gmail.com>
2023-08-18 03:59:44 -0700

    removed the spaces around the = sign

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -8,4 +8,4 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows LEFT JOIN tv_show_genres
-WHERE tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
+WHERE tv_shows.id=tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
