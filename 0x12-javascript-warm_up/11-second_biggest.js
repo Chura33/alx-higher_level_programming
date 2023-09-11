f2eb30f9666ed6c33b3f3d2205177467dbff7ca5
Chura33 <churabrady@gmail.com>
2023-09-11 15:19:05 -0700

    solved issues with return statement outside function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 
-let newArr = process.argv.slice(2);
+const newArr = process.argv.slice(2);
 
 if (newArr.length <= 1) {
-    console.log(0);
-    return;
-  }
-let intArr = newArr.map(item => parseInt(item)).filter(arg => !isNaN(arg));
+  console.log(0);
+} else {
+  let intArr = newArr.map(item => parseInt(item)).filter(arg => !isNaN(arg));
 
-if (intArr.length < 2) {
+  if (intArr.length < 2) {
     console.log(0);
-    return;
-  }
-intArr = intArr.sort((a, b)=> {
-  return (a - b);
-})
+  } else {
+    intArr = intArr.sort((a, b) => {
+      return (a - b);
+    });
 
-console.log(intArr[intArr.length - 2]);
+    console.log(intArr[intArr.length - 2]);
+  }
+}
