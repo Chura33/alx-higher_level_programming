e0935f888352db65a69cfac451d7ba627634a5be
Chura33 <churabrady@gmail.com>
2023-09-11 13:42:01 -0700

    solved task 8 without using process

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,15 @@
 #!/usr/bin/node
 
+let row = '';
 const num = parseInt(process.argv[2]);
 if (isNaN(num)) {
   console.log('Missing size');
 } else if (!isNaN(num)) {
   for (let i = 0; i < num; i++) {
     for (let j = 0; j < num; j++) {
-      process.stdout.write('x');
+      row += 'x';
     }
-    process.stdout.write('\n');
+    console.log(row);
+	row = '';
   }
 }
