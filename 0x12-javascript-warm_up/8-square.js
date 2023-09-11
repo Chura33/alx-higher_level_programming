5b4514d4236cfad948396a3c9fce6e603754e976
Chura33 <churabrady@gmail.com>
2023-09-11 12:35:30 -0700

    changed # to x

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ const num = parseInt(process.argv[2]);
 if (!isNaN(num)) {
   for (let i = 0; i < num; i++) {
     for (let j = 0; j < num; j++) {
-      process.stdout.write('#');
+      process.stdout.write('x');
     }
     process.stdout.write('\n');
   }
