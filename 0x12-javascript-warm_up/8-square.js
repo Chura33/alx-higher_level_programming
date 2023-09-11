6d4bd5803e7f540fb4eec0e72fd3f594e62b48f2
Chura33 <churabrady@gmail.com>
2023-09-11 13:32:31 -0700

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 
 const num = parseInt(process.argv[2]);
-
-if (!isNaN(num)) {
+if (isNaN(num)) {
+  console.log('Missing size');
+} else if (!isNaN(num)) {
   for (let i = 0; i < num; i++) {
     for (let j = 0; j < num; j++) {
       process.stdout.write('x');
     }
     process.stdout.write('\n');
   }
-} else {
-  console.log('Missing size');
 }
