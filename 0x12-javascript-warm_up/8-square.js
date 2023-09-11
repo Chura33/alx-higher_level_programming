dea7a78a684c98a402ce374d0c0255707004a48a
Chura33 <churabrady@gmail.com>
2023-09-11 13:49:03 -0700

    corrected the style to standard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,14 +3,13 @@
 const size = parseInt(process.argv[2]);
 
 if (!isNaN(size)) {
-    for (let i = 0; i < size; i++) {
-      let row = '';
-      for (let j = 0; j < size; j++) {
-        row += 'X';
-      }
-      console.log(row);
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
     }
-  } else {
+    console.log(row);
+  }
+} else {
   console.log('Missing size');
 }
-
