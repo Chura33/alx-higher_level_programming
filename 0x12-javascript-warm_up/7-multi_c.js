3031c38b743e1974c47c10a91aae15935a5daefe
Chura33 <churabrady@gmail.com>
2023-09-11 12:22:30 -0700

    solved isNaN issue

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const num = parseInt(process.argv[2]);
+const x = parseInt(process.argv[2]);
 
-if (isNaN(num)) {
+if (isNaN(x) === true) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < num; i++) {
+  for (let i = 0; i < x; i++) {
     console.log('c is fun');
   }
 }
