cd37181c32d06f28b9deb6c9e73ade8d74ffad4a
Chura33 <churabrady@gmail.com>
2023-09-11 13:57:52 -0700

    solved task 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,8 +3,8 @@
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
 
-function add(a,b) {
- console.log(a + b);
+function add (a, b) {
+  console.log(a + b);
 }
 
 add(a, b);
