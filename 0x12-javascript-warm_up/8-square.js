4ea5792c6f7b9836f9e258523ac8301b00009b02
Chura33 <churabrady@gmail.com>
2023-09-11 12:33:46 -0700

    solved 8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+
+if (!isNaN(num)) {
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      process.stdout.write('#');
+    }
+    process.stdout.write('\n');
+  }
+} else {
+  console.log('Missing size');
+}
