06832f518bafc2e962c625c1cb899dc338bca25e
Chura33 <churabrady@gmail.com>
2023-09-12 14:09:13 -0700

    solved 5-square.js

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rec = '';
+      for (let j = 0; j < this.width; j++) {
+        rec += 'X';
+      }
+      console.log(rec);
+    }
+  }
+
+  rotate () {
+    const w1 = this.width;
+    this.width = this.height;
+    this.height = w1;
+  }
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
