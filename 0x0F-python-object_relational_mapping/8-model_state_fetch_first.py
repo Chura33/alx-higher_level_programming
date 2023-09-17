3282ac24a0485c0a39a4b2b5b2aed5dd1a97ee93
Chura33 <churabrady@gmail.com>
2023-09-17 02:49:34 -0700

    solved task 8-model_state_fetch_first.py

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+This module  prints the
+first State object from the
+database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
+
+
+def print_first_state(user, passwd, db):
+    """
+    - prints the first State object from
+    - the database hbtn_0e_6_usa
+    """
+
+    try:  # establish a connection with db
+        engine = create_engine(
+            f'mysql+mysqldb://{user}:'
+            f'{passwd}@localhost:3306/{db}'
+        )
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        first_state = session.query(State).order_by(State.id).first()
+
+        if first_state:
+            print(f"{first_state.id}: {first_state.name}")
+        else:
+            print("Nothing")
+        session.close()
+    except SQLAlchemyError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <db_name>")
+        sys.exit(1)
+
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    print_first_state(user, passwd, db)
