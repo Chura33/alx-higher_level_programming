c4f38dcb91e9ee668ce29035426dd2ee8982472c
Chura33 <churabrady@gmail.com>
2023-09-28 10:12:06 -0700

    solved task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a GET request to a URL using curl and displays the response body for a 200 status code.
-curl -sI "$1" | grep "HTTP/1.1 200 OK" && curl -si "$1"
+curl -si 0.0.0.0:5000|grep -iq "^HTTP/1.1 200 OK" && curl -L $1
