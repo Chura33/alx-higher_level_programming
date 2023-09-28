4d7ad35f3f9dc95aad103967d41c5e6c6b3c07e8
Chura33 <churabrady@gmail.com>
2023-09-28 09:03:50 -0700

    sovled task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a GET request to a URL using curl and displays the response body for a 200 status code.
-curl -si "$1" | awk 'NR==1{if($2==200)body=1}body'
+curl -s -w "%{http_code}" "$url" | awk 'NR==1 && $1==200 {p=1} p'
