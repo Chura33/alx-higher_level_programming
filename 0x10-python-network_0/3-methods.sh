edffee03e05dd76012823954bad57d6ba5b84101
Chura33 <churabrady@gmail.com>
2023-09-28 10:46:49 -0700

    solved task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script prints all the methods that a server will accept
-curl -sX OPTIONS -I $1 | grep -i 'allow' | awk -F ':' '{print $2}'
+curl -sX OPTIONS -I $1 | grep -i 'allow' | awk -F ': ' '{print $2}'
