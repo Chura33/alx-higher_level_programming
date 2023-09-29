91dfdcff6622bb7eb66f9d8dda4b60d7bec2dc86
Chura33 <churabrady@gmail.com>
2023-09-29 09:15:34 -0700

    solved task e

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,16 +10,16 @@ import sys
 
 # Get the URL from the command-line argument
 url = sys.argv[1]
-
-try:
-    with urllib.request.urlopen(url) as response:
-        # Check if the response is an HTTP error
-        if response.getcode() != 200:
-            print("Error code:", response.getcode())
-        else:
-            res = response.read().decode('utf-8')
-            print(res)
-except urllib.error.HTTPError as e:
-    print("Error code:", e.code)
-except Exception as e:
-    print("An error occurred:", e)
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(url) as response:
+            # Check if the response is an HTTP error
+            if response.getcode() != 200:
+                print("Error code:", response.getcode())
+            else:
+                res = response.read().decode('utf-8')
+                print(res)
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
+    except Exception as e:
+        print("An error occurred:", e)
