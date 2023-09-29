0b1e2a63df5f2acb06a993cd840e10511a4deed0
Chura33 <churabrady@gmail.com>
2023-09-29 11:19:47 -0700

    solvwd task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,18 +5,13 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    # Get the URL and email from the command-line arguments
     url = sys.argv[1]
     email = sys.argv[2]
-
-    # Create a dictionary with the email parameter
     data = {'email': email}
 
     try:
-        # Send a POST request with the data
         response = requests.post(url, data=data)
 
-        # Check if the request was successful (status code 200)
         if response.status_code == 200:
             print(response.text)
         else:
