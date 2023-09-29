595c6fcfed3edee7d7e5d25199ecc8d6f41a97c4
Chura33 <churabrady@gmail.com>
2023-09-29 09:08:35 -0700

    solved task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request
+to the URL and displays
+the body of the response
+"""
+import urllib.request
+import urllib.error
+import sys
+
+# Get the URL from the command-line argument
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+        # Check if the response is an HTTP error
+        if response.getcode() != 200:
+            print("Error code:", response.getcode())
+        else:
+            res = response.read().decode('utf-8')
+            print(res)
+except urllib.error.HTTPError as e:
+    print("Error code:", e.code)
+except Exception as e:
+    print("An error occurred:", e)
