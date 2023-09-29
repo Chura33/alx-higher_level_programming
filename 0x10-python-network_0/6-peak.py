1c9a5ac9c1a20404c10a1ec67c01ff017bd495e8
Chura33 <churabrady@gmail.com>
2023-09-28 21:27:10 -0700

    solved task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -22,11 +22,3 @@ def find_peak(list_of_integers):
 
     # The peak will be at the 'left' index
     return list_of_integers[left]
-
-
-print(find_peak([1, 2, 4, 6, 3]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
-print(find_peak([2, 2, 2]))
-print(find_peak([]))
-print(find_peak([-2, -4, 2, 1]))
-print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
