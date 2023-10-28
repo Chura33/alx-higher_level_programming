304fc43fe3241454b9f0f4483e2ad4e8b850099b
Chura33 <churabrady@gmail.com>
2023-10-27 23:09:07 -0700

    solved semistandard errors

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const args = process.argv[2];
 
 fs.readFile(args, 'utf-8', (err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-      console.log(data.toString());
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
 });
